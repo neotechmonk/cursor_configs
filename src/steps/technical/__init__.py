1e7c0ea9372170549160ee069b8491d71b987920
Neo Tech Monk <your_email@gmail.com>
2025-06-08 09:53:30 +1000

    feat: expose detect_wide_range_bar as wrb in technical steps package

--- src/steps/technical/__init__.py
+++ src/steps/technical/__init__.py
@@ -2,4 +2,8 @@
 
 This package contains strategy steps that implement various technical analysis
 methods such as trend detection, momentum indicators, and volatility measures.
-""" 
+"""
+
+from .wrb import detect_wide_range_bar as wrb
+
+__all__ = ['wrb']
