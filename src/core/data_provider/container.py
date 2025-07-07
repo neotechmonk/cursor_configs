7a78a5ca6df187c5b6a3abf37536e0fadf599672
Neo Tech Monk <your_email@gmail.com>
2025-07-07 18:23:36 +1000

    incorpoate the cache mechanism

---
+++ src/core/data_provider/container.py
@@ -0,0 +1,30 @@
+
+from dependency_injector import containers, providers
+
+from core.data_provider.service import DataProviderService
+from core.data_provider.settings import DataProviderSettings
+from util.custom_cache import CacheInvalidationHandler, ScopedCacheView, WatchedCache
+
+
+class DataProviderContainer(containers.DeclarativeContainer):
+    NAMESPACE = __name__
+    settings = providers.Configuration()
+    
+    #Cache management
+    # -- Cache object
+    cache_backend = providers.Singleton(WatchedCache)
+    # -- Isolates cache for the data provider
+    scoped_cache = providers.Factory(
+        ScopedCacheView,
+        NAMESPACE,  # <-- positional
+        cache_backend)
+    observer = providers.Resource(
+        CacheInvalidationHandler.start,
+        config_dir=providers.Callable(lambda s: s.config_dir, settings),
+        cache=scoped_cache)
+    
+    service = providers.Singleton(
+        DataProviderService,
+        config_dir=providers.Callable(lambda s: s.config_dir, settings),
+        cache=scoped_cache,
+    )
