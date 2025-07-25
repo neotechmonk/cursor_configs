d1a58ff7d9c299400cad1ffb1dbc6c2b1fcac422
Neo Tech Monk <your_email@gmail.com>
2025-07-25 22:04:52 +0530

    Abstraction for Railroad Programming

---
+++ src/util/result.py
@@ -0,0 +1,86 @@
+from typing import Protocol, TypeVar, runtime_checkable
+
+from result import Err as LibErr
+from result import Ok as LibOk
+from result import Result as LibResult
+
+T = TypeVar("T")
+E = TypeVar("E")
+
+
+@runtime_checkable
+class OkProtocol(Protocol[T]):
+    @property
+    def value(self) -> T: ...
+
+
+@runtime_checkable
+class ErrProtocol(Protocol[E]):
+    @property
+    def error(self) -> E: ...
+
+
+@runtime_checkable
+class ResultProtocol(Protocol[T, E]):
+    def is_ok(self) -> bool: ...
+    def is_err(self) -> bool: ...
+    def unwrap(self) -> T: ...
+    def unwrap_err(self) -> E: ...
+
+
+class Ok():
+    """
+    implements OkProtocol[T]
+    """
+    def __init__(self, value: T):
+        self._inner = LibOk(value)
+
+    @property
+    def value(self) -> T:
+        return self._inner.ok_value
+
+    def to_result(self) -> LibResult:
+        return self._inner
+
+
+class Err():
+    """
+    implements ErrProtocol[E]
+    """
+    def __init__(self, error: E):
+        self._inner = LibErr(error)
+
+    @property
+    def error(self) -> E:
+        return self._inner.err_value
+
+    def to_result(self) -> LibResult:
+        return self._inner
+
+
+class Result():
+    """
+    implements ResultProtocol[T, E]
+    """
+    def __init__(self, result: LibResult):
+        self._inner = result
+
+    @classmethod
+    def ok(cls, value: T) -> "Result[T, E]":
+        return cls(LibOk(value))
+
+    @classmethod
+    def err(cls, error: E) -> "Result[T, E]":
+        return cls(LibErr(error))
+
+    def is_ok(self) -> bool:
+        return self._inner.is_ok()
+
+    def is_err(self) -> bool:
+        return self._inner.is_err()
+
+    def unwrap(self) -> T:
+        return self._inner.unwrap()
+
+    def unwrap_err(self) -> E:
+        return self._inner.unwrap_err()
